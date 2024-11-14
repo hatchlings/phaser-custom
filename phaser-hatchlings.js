05c56ad402c7feb12800925c96332b8798857b3d
Steven Lischer <885108+TomorrowToday@users.noreply.github.com>
2024-11-14 15:35:40 -0600

    chopping away

--- phaser-hatchlings.js
+++ phaser-hatchlings.js
@@ -14,23 +14,22 @@ var Extend = require('utils/object/Extend');
  */
 
 var Phaser = {
-
-    Actions: require('actions'),
-    Animations: require('animations'),
+    // Actions: require('actions'),
+    // Animations: require('animations'),
     // BlendModes: require('renderer/BlendModes'),
-    Cache: require('cache'),
+    // Cache: require('cache'),
     Cameras: {
         Scene2D: require('cameras/2d')
     },
     Core: require('core'),
     Class: require('utils/Class'),
-    Create: require('create'),
+    // Create: require('create'),
     // Curves: require('curves'),
     // Data: require('data'),
-    Display: require('display'),
+    // Display: require('display'),
     // DOM: require('dom'),
     Events: require('events/index'),
-    // FX: require('fx'),
+    FX: require('fx'),
     Game: require('core/Game'),
     GameObjects: {
         DisplayList: require('gameobjects/DisplayList'),
@@ -38,30 +37,42 @@ var Phaser = {
         GameObjectFactory: require('gameobjects/GameObjectFactory'),
         UpdateList: require('gameobjects/UpdateList'),
         Components: require('gameobjects/components'),
+        Container: require('gameobjects/container/Container'),
         BuildGameObject: require('gameobjects/BuildGameObject'),
         BuildGameObjectAnimation: require('gameobjects/BuildGameObjectAnimation'),
         GameObject: require('gameobjects/GameObject'),
         Graphics: require('gameobjects/graphics/Graphics'),
         Group: require('gameobjects/group/Group'),
         Image: require('gameobjects/image/Image'),
-        Layer: require('gameobjects/layer/Layer'),
+        // Layer: require('gameobjects/layer/Layer'),
         Particles: require('gameobjects/particles'),
         Sprite: require('gameobjects/sprite/Sprite'),
         Text: require('gameobjects/text/Text'),
+
+        // Shapes
+        Shape: require('gameobjects/shape/Shape'),
+        Rectangle: require('gameobjects/shape/rectangle/Rectangle'),
+
         Factories: {
+            Container: require('gameobjects/container/ContainerFactory'),
             Graphics: require('gameobjects/graphics/GraphicsFactory'),
             Group: require('gameobjects/group/GroupFactory'),
             Image: require('gameobjects/image/ImageFactory'),
-            Layer: require('gameobjects/layer/LayerFactory'),
+            // Layer: require('gameobjects/layer/LayerFactory'),
             Particles: require('gameobjects/particles/ParticleEmitterFactory'),
             Sprite: require('gameobjects/sprite/SpriteFactory'),
             Text: require('gameobjects/text/TextFactory'),
+
+            // Shapes
+            Rectangle: require('gameobjects/shape/rectangle/RectangleFactory'),
         },
+
         Creators: {
+            Container: require('gameobjects/container/ContainerCreator'),
             Graphics: require('gameobjects/graphics/GraphicsCreator'),
             Group: require('gameobjects/group/GroupCreator'),
             Image: require('gameobjects/image/ImageCreator'),
-            Layer: require('gameobjects/layer/LayerCreator'),
+            // Layer: require('gameobjects/layer/LayerCreator'),
             Particles: require('gameobjects/particles/ParticleEmitterCreator'),
             Sprite: require('gameobjects/sprite/SpriteCreator'),
             Text: require('gameobjects/text/TextCreator'),
@@ -71,7 +82,19 @@ var Phaser = {
         Circle: require('geom/circle'),
         Rectangle: require('geom/rectangle'),
     },
-    Input: require('input'),
+    Input: {
+        CreatePixelPerfectHandler: require('input/CreatePixelPerfectHandler'),
+        CreateInteractiveObject: require('input/CreateInteractiveObject'),
+        Events: require('input/events'),
+        // GamePad: require('input/gamepad'),
+        InputManager: require('input/InputManager'),
+        InputPlugin: require('input/InputPlugin'),
+        InputPluginCache: require('input/InputPluginCache'),
+        Keyboard: require('input/keyboard'),
+        Mouse: require('input/mouse'),
+        Pointer: require('input/Pointer'),
+        Touch: require('input/touch')
+    },
     Loader: {
         FileTypes: {
             AnimationJSONFile: require('loader/filetypes/AnimationJSONFile'),
@@ -99,9 +122,28 @@ var Phaser = {
     },
     Math: require('math'),
     Physics: {
-        Arcade: require('physics/arcade')
+        Arcade: {
+            ArcadePhysics: require('physics/arcade/ArcadePhysics'),
+            Body: require('physics/arcade/Body'),
+            Collider: require('physics/arcade/Collider'),
+            Components: require('physics/arcade/components'),
+            Events: require('physics/arcade/events'),
+            Factory: require('physics/arcade/Factory'),
+            GetCollidesWith: require('physics/arcade/GetCollidesWith'),
+            GetOverlapX: require('physics/arcade/GetOverlapX'),
+            GetOverlapY: require('physics/arcade/GetOverlapY'),
+            SeparateX: require('physics/arcade/SeparateX'),
+            SeparateY: require('physics/arcade/SeparateY'),
+            Group: require('physics/arcade/PhysicsGroup'),
+            Image: require('physics/arcade/ArcadeImage'),
+            Sprite: require('physics/arcade/ArcadeSprite'),
+            StaticBody: require('physics/arcade/StaticBody'),
+            StaticGroup: require('physics/arcade/StaticPhysicsGroup'),
+            // Tilemap: require('physics/arcade/Tilemap'),
+            World: require('physics/arcade/World')
+        }
     },
-    Plugins: require('plugins'),
+    // Plugins: require('plugins'),
     Renderer: require('renderer'),
     Scale: require('scale'),
     ScaleModes: require('renderer/ScaleModes'),
@@ -110,9 +152,9 @@ var Phaser = {
     // Structs: require('structs'),
     Textures: require('textures'),
     // Tilemaps: require('tilemaps'),
-    Time: require('time'),
+    // Time: require('time'),
     Tweens: require('tweens'),
-    Utils: require('utils')
+    // Utils: require('utils')
 
 };
 
